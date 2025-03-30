4f53f8e77e511d5b860b777d0781f215786bcce2
eRHaDev <28397119+eRHaDev@users.noreply.github.com>
2025-03-30 11:37:59 +0200

    fix line splitting

--- src/main/java/dev/digitality/digitalgui/api/InteractiveItem.java
+++ src/main/java/dev/digitality/digitalgui/api/InteractiveItem.java
@@ -122,7 +122,7 @@ public class InteractiveItem extends ItemStack {
                 List<String> loreList = new ArrayList<>();
 
                 for (String line : lore)
-                    loreList.addAll(Arrays.asList(line.split("\n")));
+                    loreList.addAll(Arrays.asList(line.split("[\n\r]|\\n|\\r")));
 
                 meta.setLore(loreList);
             }
